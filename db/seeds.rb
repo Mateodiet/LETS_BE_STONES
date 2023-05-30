70dd282601d9515dce0448d25ddde6e8a44aeee4
victor lebret <lebret.victor02@gmail.com>
2023-05-30 18:03:26 +0200

    content homepage meilleures ventes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ p "#{User.count} user created"
 Stone.destroy_all
 p "all stones destroyed"
 stone_1 = Stone.create(name:'labradorite', description: 'ça aide les genoux', price: 5.99, user: user)
-stone_2 = Stone.create(name:'caillou 1', description: 'ça aide les pieds', price: 3.99, user: user)
+stone_2 = Stone.create(name:'emeraude', description: 'ça aide les pieds', price: 3.99, user: user)
 stone_3 = Stone.create(name:'caillou 2', description: 'ça aide la tête', price: 9.99, user: user)
 stone_4 = Stone.create(name:'caillou 3', description: 'ça aide les epaules', price: 4.99, user: user)
 stone_5 = Stone.create(name:'caillou 4', description: 'ça aide le cul', price: 6.99, user: user)
